e0d3713c306c614bccd9144923466a52590f54fc
friendzz <whycantwebefriendzz@gmail.com>
2022-03-27 09:05:38 -0400

    Server starts, listens, accepts a packet, and returns.

--- chat-server/src/server.c
+++ chat-server/src/server.c
@@ -35,11 +35,45 @@ int main()
 
     // init server_addr to empty
     memset (&server_addr, 0, sizeof (server_addr));
-
     // set family, address, and port
     server_addr.sin_family      = AF_INET;
     server_addr.sin_addr.s_addr = htonl (INADDR_ANY);
     server_addr.sin_port        = htons (PORT);
+
+    if (bind (server_sock, (struct sockaddr *)&server_addr, sizeof (server_addr)) < 0) 
+    {
+        logger ("bind() FAILED");
+        close (server_sock);
+        return 2;
+    }
+    logger ("bind() successful");
+
+    if (listen (server_sock, 5) < 0) 
+    {
+        logger ("listen() - FAILED.\n");
+        close (server_sock);
+        return 3;
+    }
+    logger ("listen() successful\n");
+
+    int i=0;
+    while(i == 0)
+    {
+        logger("Ready to accept()");
+        fflush(stdout);	
+        client_len = sizeof (client_addr);
+        if ((client_sock = accept (server_sock,(struct sockaddr *)&client_addr, &client_len)) < 0) 
+        {
+            logger("accept() FAILED");
+            fflush(stdout);	
+            return 4;
+        }
+
+        logger ("received a packet from CLIENT");
+        fflush(stdout);	
+
+        i++;
+    }
 
     close(server_sock);
 
